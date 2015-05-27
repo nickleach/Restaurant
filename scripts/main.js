33149994464ff622e9266ef332451d5202d09bc7
Meade Slocomb <meade.slocomb@gmail.com>
2015-05-27 15:24:49 -0400

    Established API variables, started basic outline of script

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,46 @@
-console.log('This is cool!');
+// $(document).ready( function () {
+
+var getNews = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/news/latest');
+
+var getSpecials = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu/special');
+
+var getMenu = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu-1');
+
+
+
+
+var newReservation = {
+  fullName: '',
+  numberofGuests: '',
+  date: '',
+  specialNotes: '',
+  seatingPref: ''
+}
+
+var templateString = $('#reservationFormatting').text();
+
+var templateFunction = _.template(templateString);
+
+var finalHTML = templateFunction(newReservation);
+
+function formBuilder(data) {
+
+  var formHTML = $('#formElement').text();
+
+  var formFunction = _.template(formHTML);
+
+  data.forEach( function(elem) {
+    $('#reservationForm').append(formFunction(elem));
+
+
+  });
+
+
+}
+
+
+
+
+
+
+// })
