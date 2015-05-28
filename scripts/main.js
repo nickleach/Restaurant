39f9f0425e7d1055dd895b62f74685a44afcbb49
Meade Slocomb <meade.slocomb@gmail.com>
2015-05-28 09:10:19 -0400

    Minor tweaks to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
 // $(document).ready( function () {
-newsURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/news/latest';
+  newsURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/news/latest';
 
-$.getJSON(newsURL).success(function(response) {
-  $('.news').html('<span>' + response.title + '</span><span>' + response.date_published + '</span><p>' + response.post + '</p>');
-}
-  );
+  $.getJSON(newsURL).success(function(response) {
+    $('.news').html('<span>' + response.title + '</span><span>' + response.date_published + '</span><p>' + response.post + '</p>');
+  }
+    );
 
-specialURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu/special';
-menuURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu-1';
+  specialURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu/special';
+  menuURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu-1';
 
-var getMenu = $.getJSON(menuURL).success(function(response) {
-  $('#menu').append($.each(response, function(key,value) {
-    '<h2>' + key + '</h2>';
-  }))
-})
+  var getMenu = $.getJSON(menuURL).success(function(response) {
+    $('#menu').append($.each(response, function(key,value) {
+      '<h2>' + key + '</h2>';
+    }))
+  })
 
 
-$('#menu').on ('click', function() {
-  $('.story').removeClass('displayedsection');
-  $('.reserve').removeClass('displayedsection');
-  $('.menu').addClass('displayedsection')
-});
+  $('#menu').on ('click', function() {
+    $('.story').removeClass('displayedsection');
+    $('.reserve').removeClass('displayedsection');
+    $('.menu').addClass('displayedsection')
+  });
 
-$('#story').on ('click', function() {
-  $('.menu').removeClass('displayedsection');
-  $('.reserve').removeClass('displayedsection');
-  $('.store').addClass('displayedsection')
-});
+  $('#story').on ('click', function() {
+    $('.menu').removeClass('displayedsection');
+    $('.reserve').removeClass('displayedsection');
+    $('.store').addClass('displayedsection')
+  });
 
-$('#reserve').on ('click', function() {
-  $('.story').removeClass('displayedsection');
-  $('.menu').removeClass('displayedsection');
-  $('.reserve').addClass('displayedsection')
-});
+  $('#reserve').on ('click', function() {
+    $('.story').removeClass('displayedsection');
+    $('.menu').removeClass('displayedsection');
+    $('.reserve').addClass('displayedsection')
+  });
 
 
 // (function(response) {
