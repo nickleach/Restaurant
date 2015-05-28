381ede4e30c9c7cec68f8ebce0f146824084f1d6
Meade Slocomb <meade.slocomb@gmail.com>
2015-05-27 21:06:40 -0400

    Completed JS to populate Latest News div

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
 // $(document).ready( function () {
+newsURL = 'http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/news/latest';
 
-var getNews = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/news/latest');
+var getNews = $.getJSON(newsURL).success(function(response) {
+
+  $('.news').html('<span>' + response.title + '</span><span>' + response.date_published + '</span><p>' + response.post + '</p>');
+}
+  );
 
 var getSpecials = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu/special');
 
 var getMenu = $.getJSON('http://private-anon-fb178ad55-restaurantapi.apiary-mock.com/menu-1');
+
+var getMainPic = $.getJSON('http://api.flickr.com/services/rest/?');
+
+
+var news = getNews.response;
+
+
+
 
 
 
